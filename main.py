b9238c0fd075b3e239694e91df6c42bc9923612b
Geraetefreund <friteuse@arcor.de>
2024-09-02 12:38:08 +0100

    add player.py

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
+    player = Player((SCREEN_WIDTH /2), (SCREEN_HEIGHT /2))
     dt = 0
+
     while(1):
         """ This will make the windows close button work. """
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                print("Exiting Asteroids...")
                 return
-        screen.fill(color="black")
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the FPS to 60
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
