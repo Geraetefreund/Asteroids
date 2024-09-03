bcffcebba09cb9e1cb4a544acb9680285c6f3229
Geraetefreund <friteuse@arcor.de>
2024-09-03 08:00:53 +0100

    creating and using groups

--- main.py
+++ main.py
@@ -7,8 +7,17 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player((SCREEN_WIDTH /2), (SCREEN_HEIGHT /2))
+
     dt = 0
+
+
 
     while(1):
         """ This will make the windows close button work. """
@@ -16,10 +25,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # limit the FPS to 60
