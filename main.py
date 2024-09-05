81dd6820fb1af1520c9451cab35747c7a0acc585
Geraetefreund <friteuse@arcor.de>
2024-09-05 06:16:41 +0100

    add pew pew

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import *
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
@@ -14,7 +15,9 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
+    Shot.containers = (shots, updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
@@ -26,7 +29,6 @@ def main():
     dt = 0
 
 
-
     while(1):
         """ This will make the windows close button work. """
         for event in pygame.event.get():
@@ -36,10 +38,16 @@ def main():
         for obj in updatable:
             obj.update(dt)
 
+
         for asteroid in asteroids:
             if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit(0)
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.kill()
+                    shot.kill()
 
         screen.fill("black")
 
